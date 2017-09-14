d4759b2697299abe56538444d033f341512d6858
Cory Logan <he@corylogan.com>
2017-09-14 15:14:53 -0700

    initial, demonstrating my problem

---
+++ src/renderer/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="main">
+    <h1>Main View</h1>
+    <jester />
+  </div>
+</template>
+
+<script>
+  import Jester from './jester.vue'
+
+  export default {
+    name: 'app',
+    components: { jester: Jester },
+    data () {
+      return {
+      }
+    }
+}
+</script>
---
+++ src/renderer/jester.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>This is the Jester component {{something}}</p>
+</template>
+
+<script>
+export default {
+  name: 'jester',
+  data () {
+    return {
+      foo: 'bar'
+    }
+  }
+}
+</script>
